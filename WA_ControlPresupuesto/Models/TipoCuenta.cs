a409afd3d38883f170f0cb7b99f9f58eb135980b
David Nava <david.nava.garcia4@gmail.com>
2025-08-31 21:58:56 -0600

    Validations by models

--- WA_ControlPresupuesto/Models/TipoCuenta.cs
+++ WA_ControlPresupuesto/Models/TipoCuenta.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
+using WA_ControlPresupuesto.Validations;
 
 namespace WA_ControlPresupuesto.Models
 {
@@ -8,27 +9,36 @@ namespace WA_ControlPresupuesto.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [StringLength(maximumLength:50, MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]
         [Display(Name ="Nombre del tipo cuenta")]
-
+        [PrimeraLetraMayusculas]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden {  get; set; }
 
         /*Pruebas de otras validaciones por defecto*/
-        [Required(ErrorMessage = "El campo {0} es requerido")]
-        [EmailAddress(ErrorMessage ="El campo debe ser un correo electrónico válido")]
-        public string Email { get; set; }
 
-        [Range(minimum:18, maximum:130, ErrorMessage ="El valor debe estar entre {1} y {2}")]
-        public int Edad {  get; set; }
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        //[Display(Name = "Tipo cuenta")]
+        //public string Tipo { get; set; }
 
-        [Url(ErrorMessage ="El campo debe ser una URL válida")]
-        public string URL { get; set; }
 
-        [CreditCard(ErrorMessage ="La tarjeta de crédito no es válida")]
-        [Display(Name = "Tarjeta de Crédito")]
-        public string TarjetaDeCredito {  get; set; }
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage ="El campo debe ser un correo electrónico válido")]
+        //public string Email { get; set; }
+
+        //[Range(minimum:18, maximum:130, ErrorMessage ="El valor debe estar entre {1} y {2}")]
+        //public int Edad {  get; set; }
+
+        //[Url(ErrorMessage ="El campo debe ser una URL válida")]
+        //public string URL { get; set; }
+
+        //[CreditCard(ErrorMessage ="La tarjeta de crédito no es válida")]
+        //[Display(Name = "Tarjeta de Crédito")]
+        //public string TarjetaDeCredito {  get; set; }
+
+
+
 
     }
 }
