9155792060387bd02f5fdeadc89f87f78e38af6c
David Nava <david.nava.garcia4@gmail.com>
2025-09-06 18:34:38 -0600

    Add Modulo Cuentas

---
+++ WA_ControlPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WA_ControlPresupuesto.Models;
+using WA_ControlPresupuesto.Services;
+
+namespace WA_ControlPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var modelo = new CuentaCreacionViewModel();
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            //El primer valor es el texto y el segundo es el valor. En este caso el nombre es el display y el valor a pasar es el id
+            return View(modelo);
+        }
+    }
+}
---
+++ WA_ControlPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WA_ControlPresupuesto.Models
+{
+    public class CuentaCreacionViewModel:Cuenta
+    {
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }//Clase especiales de ASP.NET CORE que nos permite crear listas desplegables en las vistas de Razor Pages o MVC Views de una manera muy sencilla
+    }
+}
